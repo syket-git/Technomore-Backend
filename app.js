e573a3582f7bf537670fb529b7868c064ab7a828
syket <syketweb@gmail.com>
2022-01-25 11:00:33 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const config = require('./config');
+
+const app = express();
+
+const { PORT } = config;
+
+//** Initialize middleware
+app.use(express.json());
+dotenv.config();
+
+//** Connect with mongodb
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING)
+  .then(() => {
+    console.log('DB connected ');
+  })
+  .catch((err) => console.log('DB connection error'));
+
+//** Application Route
+
+app.use('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'Welcome to technomore backend',
+  });
+});
+
+//** Run server */
+app.listen(PORT, () => console.log(`Server up and running on ${PORT}`));
---
+++ config/index.js
@@ -0,0 +1,5 @@
+const config = {
+  PORT: 5000,
+};
+
+module.exports = config;
